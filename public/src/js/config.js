7f283d9640791ad054d4004611fe932fa52bfa15
nidscom <0x0@ciphs.org>
2021-05-12 02:46:12 +0800

    Initial Commit

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -5,7 +5,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Komodo Block '
+      title: 'Ciphscoin Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -17,7 +17,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Komodo Transaction '
+      title: 'Ciphscoin Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -25,15 +25,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Komodo Blocks solved Today'
+      title: 'Ciphscoin Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Komodo Blocks solved '
+      title: 'Ciphscoin Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Komodo Address '
+      title: 'Ciphscoin Address '
     }).
     when('/charts/:chartType?', {
       templateUrl: 'views/charts.html',
